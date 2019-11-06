ae3d0375e3fbbbaae1129e9d15e9051d799312bd
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-06 15:43:24 -0800

    Add Outcomes component

---
+++ src/Outcomes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Outcomes.css';
+
+class Outcomes extends Component {
+  render() {
+    return (
+        <div>
+          <p>Out of x flips there were:</p>
+        </div>
+    )
+  }
+}
+
+export default Outcomes;
