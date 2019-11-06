3fc10fe555c426071b0522b6dd5ac52fc83280b7
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-06 13:35:04 -0800

    Add Coin component

---
+++ src/Coin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Coin extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+        <div>
+          <h2>Coin Flip Fun</h2>
+        </div>
+    )
+  }
+}
+
+export default Coin;
